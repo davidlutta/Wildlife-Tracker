1d88ea8e62ab1d113c71799a34edf21b149110a0
davidlutta <davidlutta2010@gmail.com>
2018-05-25 16:35:53 +0300

    Add test to check if RegAnimal instantiates

--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -1,5 +1,17 @@
+import org.junit.Rule;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class AnimalTest{
 
+    //Database rule
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+
+    //test to see if animal instantiates
+    @Test
+    public void animal_InstantiatesCorrectly_true(){
+        RegAnimal myAnimal = new RegAnimal("panther","matrue","healthy");
+        assertEquals(true, myAnimal instanceof RegAnimal);
+    }
 }
