4d6b468e2e84ed58642e352f269de4282c1efeb8
davidlutta <davidlutta2010@gmail.com>
2018-05-25 15:17:33 +0300

    Add Abstract class with fields and get methods

---
+++ src/main/java/Animal.java
@@ -0,0 +1,26 @@
+public abstract class Animal {
+    public String name;
+    public int age;
+    public String species;
+    public String health;
+
+    //Method to get Name of Animal
+    public String getName() {
+        return name;
+    }
+
+    //Method to get Age of Animal
+    public int getAge() {
+        return age;
+    }
+
+    //Method to get health of animal
+    public String getHealth() {
+        return health;
+    }
+
+    //Method to get Species
+    public String getSpecies() {
+        return species;
+    }
+}
