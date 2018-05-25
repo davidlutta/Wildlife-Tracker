7f01644b5081431d5583526c9bd36f33896d1ef1
davidlutta <davidlutta2010@gmail.com>
2018-05-25 15:52:15 +0300

    Make database and test database, make connection to main database

---
+++ src/test/java/DatabaseRule.java
@@ -0,0 +1,11 @@
+import org.junit.rules.ExternalResource;
+
+public class DatabaseRule extends ExternalResource {
+
+    //Making a connection to our test database before running our tests
+    @Override
+    public void before(){
+
+    }
+
+}
