cda1cde74b040d4df1147b76efdb66355b452afc
davidlutta <davidlutta2010@gmail.com>
2018-05-26 18:49:30 +0300

    Add test to check if Object is saved in database failed

--- src/main/java/Sighting.java
+++ src/main/java/Sighting.java
@@ -1,2 +1,25 @@
 public class Sighting {
+    private String name;
+    private String location;
+    private int animalId;
+    private String species;
+
+    public Sighting(String name, String location, int animalId, String species){
+        //Exception for name
+        if (name.equals("")){
+            throw new IllegalArgumentException("Please enter a name mate");
+        }
+        //Exception for location
+        if (location.equals("")){
+            throw new IllegalArgumentException("Please enter a location mate");
+        }
+        //Exception for species
+        if (species.equals("")){
+            throw new IllegalArgumentException("Please enter the species of the animal");
+        }
+        this.name = name;
+        this.location = location;
+        this.animalId = animalId;
+        this.species = species;
+    }
 }
--- src/test/java/SightingTest.java
+++ src/test/java/SightingTest.java
@@ -10,7 +10,15 @@ public class SightingTest {
     //Test to check if sighting instantiates correctly
     @Test
     public void Sighting_InstantiatesCorrectly_True(){
-        Sighting newsighting = new Sighting("Roger","Zone 1","endangered",1);
-        assertTrue(true, newsighting instanceof Sighting);
+        Sighting newsighting = new Sighting("Roger","Zone 1",1,"endangered");
+        assertTrue( newsighting instanceof Sighting);
+    }
+
+    //Test to check if sighting instantiates With Name
+    @Test
+    public void Sighting_IsSavedOnDataBase_True(){
+        Sighting newSighting = new Sighting("Roger","Zone 2",1,"endangered");
+        newSighting.save();
+        assertTrue(Sighting.all().get(0).equals(newSighting));
     }
 }
